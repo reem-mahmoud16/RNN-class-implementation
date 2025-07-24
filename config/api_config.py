edb20f15b4abf8b1515055ae3637029fad85fea3
reem-mahmoud16 <66190640+reem-mahmoud16@users.noreply.github.com>
2025-07-24 18:10:58 +0300

    Add files via upload

---
+++ config/api_config.py
@@ -0,0 +1,4 @@
+API_KEY = "AIzaSyAaza3aeaJhLzeXS48HEbPza_cElE3qU3o"
+MODEL = "gemini-1.5-flash"
+GEMINI_EMBEDDING_MODEL = "gemini-embedding-exp-03-07"
+SENTENCE_TRANSFORMERS_MODEL = 'sentence-transformers/all-MiniLM-L6-v2'
